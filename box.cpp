4ddd896931ba879d69fd4d909453c6c795e9d665
Emily Klapper <emiklap@gmail.com>
2022-09-21 14:48:03 -0400

    Updated for Lab 4-B

--- box.cpp
+++ box.cpp
@@ -22,12 +22,22 @@ void checkerboard(int w, int h) {
     std::cout << "Input height: ";
     std::cin >> h;*/
 
-    for (int i = 0; i < (h/2); i++) {
+    for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
-            if (j == w/2) {
-                std::cout << "\n ";
+            if (i % 2 == 0) {
+                if (j % 2 == 0) {   //if it is an even row, start with astericks and then spaces
+
+                    std::cout << "*";
+                } else {
+                    std::cout << " ";
+                }
+            } else {    //if it is an odd row, start with spaces and then use astericks
+                if (j % 2 == 0) {
+                    std::cout << " ";
+                } else {
+                    std::cout << "*";
+                }
             }
-            std::cout << "* ";
         }
         std::cout << "\n";
     }
--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@ int main() {
     std::cout << "Task A \nbox(7, 4)" << std::endl;
     box(7, 4);
 
-    std::cout << "Task B \ncheckerboard(11, 6)" << std::endl;
+    std::cout << "\n \nTask B \ncheckerboard(11, 6)" << std::endl;
     checkerboard(11, 6);
+    std::cout << "\ncheckerboard(11, 7)" << std::endl;
+    checkerboard(10, 7);
 
     return 0;
 }
